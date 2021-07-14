6ab4bff72915f7510356dccd10a7f8b25208f85d
philenius <philenius@users.noreply.github.com>
2021-07-14 20:29:17 +0200

    implement example Flutter app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker_desktop/file_picker_desktop.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,109 +6,242 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Native File Picker for Desktop',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.grey.shade800,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<String> _selection = [];
+  bool _multipleFiles = false;
+  FileType _fileType = FileType.any;
+  bool _isAllowedFileTypesInputVisible = false;
+  TextEditingController _allowedFileTypesController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> _pickDirectory() async {
+    try {
+      final String? selectedDirectory = await getDirectoryPath(
+        dialogTitle: 'Please select a directory:',
+      );
+      if (selectedDirectory != null) {
+        setState(() {
+          this._selection = [selectedDirectory];
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content:
+                Text('User closed the dialog without selecting a directory.'),
+          ),
+        );
+        setState(() {
+          this._selection = [];
+        });
+      }
+    } catch (e) {
+      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: Text('Exception: $e'),
+        ),
+      );
+      setState(() {
+        this._selection = [];
+      });
+    }
+  }
+
+  Future<void> _pickFile() async {
+    final allowedFileTypes = this
+        ._allowedFileTypesController
+        .text
+        .split(',')
+        .map((fileType) => fileType.trim())
+        .toList();
+
+    try {
+      final FilePickerResult? result = await pickFiles(
+        allowMultiple: this._multipleFiles,
+        allowedExtensions:
+            this._fileType == FileType.custom ? allowedFileTypes : null,
+        type: this._fileType,
+      );
+
+      if (result != null) {
+        setState(() {
+          this._selection = result.files.map((e) => e.path ?? 'ERROR').toList();
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('User closed the dialog without selecting a file.'),
+          ),
+        );
+        setState(() {
+          this._selection = [];
+        });
+      }
+    } catch (e) {
+      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: Text('Exception: $e'),
+        ),
+      );
+      setState(() {
+        this._selection = [];
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter Native File Picker for Desktop'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: EdgeInsets.all(25),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Text('Select multiple files:'),
+                SizedBox(
+                  width: 10,
+                ),
+                Switch(
+                  value: _multipleFiles,
+                  onChanged: (newValue) {
+                    setState(() {
+                      this._multipleFiles = newValue;
+                    });
+                  },
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              children: [
+                Text('Filter for file types:'),
+                SizedBox(
+                  width: 10,
+                ),
+                DropdownButton<FileType>(
+                  value: _fileType,
+                  iconSize: 24,
+                  elevation: 16,
+                  onChanged: (FileType? newValue) {
+                    setState(() {
+                      _fileType = newValue!;
+                      this._isAllowedFileTypesInputVisible =
+                          this._fileType == FileType.custom;
+                    });
+                  },
+                  items: FileType.values
+                      .map(
+                        (fileType) => DropdownMenuItem<FileType>(
+                          child: Text(fileType.toString()),
+                          value: fileType,
+                        ),
+                      )
+                      .toList(),
+                ),
+                SizedBox(
+                  width: 32,
+                ),
+                _isAllowedFileTypesInputVisible
+                    ? Expanded(
+                        child: TextField(
+                          controller: this._allowedFileTypesController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Allowed file types:',
+                            helperText:
+                                'Enter a comma-separated list of allowed file extensions (without asterisks and without dots)',
+                            hintText: 'png, jpeg, bmp',
+                          ),
+                        ),
+                      )
+                    : SizedBox(),
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ElevatedButton(
+                  onPressed: this._pickDirectory,
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Pick a directory',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: this._pickFile,
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      _multipleFiles ? 'Pick files' : 'Pick a file',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              height: 50,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Selection:',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            IntrinsicWidth(
+              child: Container(
+                height: 400,
+                child: this._selection.isEmpty
+                    ? Text('Nothing selected')
+                    : ListView.builder(
+                        itemCount: this._selection.length,
+                        itemBuilder: (BuildContext context, int index) =>
+                            ListTile(
+                          title: Text(this._selection[index]),
+                          leading: Icon(Icons.folder),
+                        ),
+                      ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
