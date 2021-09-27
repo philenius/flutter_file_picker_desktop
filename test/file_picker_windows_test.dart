4b4867a4c011dc220049596eab1142b4e29f493c
philenius <philenius@users.noreply.github.com>
2021-09-27 21:03:24 +0200

    fix test for custom file extension filter

--- test/file_picker_windows_test.dart
+++ test/file_picker_windows_test.dart
@@ -48,12 +48,12 @@ void main() {
 
       expect(
         picker.fileTypeToFileFilter(FileType.custom, ['dart']),
-        equals('Files (*.dart)\x00\x00'),
+        equals('Files (*.dart)\x00*.dart\x00\x00'),
       );
 
       expect(
         picker.fileTypeToFileFilter(FileType.custom, ['dart', 'html']),
-        equals('Files (*.dart,*.html)\x00\x00'),
+        equals('Files (*.dart,*.html)\x00*.dart;*.html\x00\x00'),
       );
     });
   });
